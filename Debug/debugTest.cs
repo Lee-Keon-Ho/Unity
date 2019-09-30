649195c6eeb31a3f8b0964d4e4179d133bcca109
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2019-09-30 16:24:00 +0900

    Create debugTest.cs

---
+++ Debug/debugTest.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class debugTest : MonoBehaviour {
+
+  int i = 0;
+  
+  void Start () {
+    Debug.Log (i++); // 시작과 동시에 단 한번만
+  }
+  
+  
+  void Update () {
+    Debug.Log (i++); // 매 프레임 마다 호출됨
+    
+    /*
+    Vector3 pos;
+    pos = this.gameObject.transform.position;
+    
+    Debug.Log (pos); // 매 프레임 마다 position 정보 출력
+    */
+  }
