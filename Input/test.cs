9aec04d6bf4835cb98960c2130a425777474f4bc
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2019-10-06 14:51:56 +0900

    Update test.cs

--- Input/test.cs
+++ Input/test.cs
@@ -9,7 +9,7 @@ public class sphere_reset : MonoBehaviour
     {
         // 시작과 동시에 단 한번만 진행합니다.
         
-        if(Input.GetKey (KeyCode.Alpha2)){
+        if(Input.GetKey (KeyCode.Alpha2)){ 
             Debug.Log ("2(@) is pressed!!"); // 키보드 2를 입력시 log창에 "2(@) is pressed!!" 출력이지만 Start에 있어서 미출력
         }
     }
@@ -24,7 +24,11 @@ public class sphere_reset : MonoBehaviour
         }
         
         if(Input.GetMouseButtonUp (1)){
-            Debug.Log ("Right mouse button is triggered!!");  // 추가 정리 필요
+            Debug.Log ("Right mouse button is triggered!!");  // 마우스 오른쪽 버튼에서 손을 땔때 동작
+        }
+        
+        if(Input.GetMouseButtonUp(1)){
+            Debug.Log(Input.mousePosition); // 화면의 좌표값을 보여준다
         }
     }
 }
