882b9d21ce297618291340302d53032f1881e02d
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2019-10-06 15:44:43 +0900

    Update object_transform.cs

--- Game Object/object_transform.cs
+++ Game Object/object_transform.cs
@@ -15,22 +15,26 @@ public class sphere_reset : MonoBehaviour
     {
         // 매 프레임 마다 진행
         if(Input.GetKeyDown (KeyCode.A)){ // A키를 누르면 동작
-          float rnd = Random.Range (-0.2f, 0.2f); // 랜덤 함수 사용
-          this.transform.position += new Vecctor3 (rnd, rnd, rnd); // 램덤으로 받은 rnd를 이용해서 *이동*
+            float rnd = Random.Range (-0.2f, 0.2f); // 랜덤 함수 사용
+            this.transform.position += new Vecctor3 (rnd, rnd, rnd); // 램덤으로 받은 rnd를 이용해서 *이동*
         }
         
         if(Input.GetKeyDown (KeyCode.B)) { // B키를 누르면 동작
-          float rnd = Random.Range (0.0f, 360.0f); // 랜덤 함수 사용
-          this.transform.rotation = Quaternion.Euler (rnd, 0.0f, 0.0f); // 랜덤으로 받은 rnd를 *rotation x값에 적용*
+            float rnd = Random.Range (0.0f, 360.0f); // 랜덤 함수 사용
+            this.transform.rotation = Quaternion.Euler (rnd, 0.0f, 0.0f); // 랜덤으로 받은 rnd를 *rotation x값에 적용*
         }
         
         if(Input.GetKeyDown (KeyCode.C)) { // C키를 누르면 동작
-          float rnd = Random.Range (0.5f, 1.5f); // 랜덤 함수 사용
-          this.transform.localScale = new Vector3 (rnd, rnd, rnd); // 랜덤으로 받은 rnd를 *localScale 값에 적용*
+            float rnd = Random.Range (0.5f, 1.5f); // 랜덤 함수 사용
+            this.transform.localScale = new Vector3 (rnd, rnd, rnd); // 랜덤으로 받은 rnd를 *localScale 값에 적용*
         }
         
         if(Input.GetKey(KeyCode.UpArrow)) { // 방향키 위
-          this.transform.Translate(new Vector3(0.0f, 0.0f, 3.0f * Time.deltaTime)); // z축 방향으로 이동
+            this.transform.Translate(new Vector3(0.0f, 0.0f, 3.0f * Time.deltaTime)); // z축 방향으로 이동
+        }
+        
+        if(Input.GetKey(KeyCode.UpArrow)) { // 방향키 위
+            this.transform.Translate(Vector3.forward * 3.0f * Time.deltaTime); // 앞으로 이동 (0, 0, 1) * 3
         }
     }
 }
