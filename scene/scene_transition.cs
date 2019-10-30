eae796429b05d6ea9ad3ba1a240fc7f5b95cde20
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2019-10-30 19:41:46 +0900

    Update scene_transition.cs

--- scene/scene_transition.cs
+++ scene/scene_transition.cs
@@ -6,13 +6,16 @@ public class scene_transition : MonoBehaviour {
     private static string msg_text;
 	
 	//Use this for initialization
-	void start () {
+	void start () 
+	{
 		
 	}
 	
 	//Update is called once per frame
-	void Update () {
-		if(Input.GetMousebuttonDown(0)){
+	void Update ()
+	{
+		if(Input.GetMousebuttonDown(0))
+		{
 			SceneManager.LoadScene ("Scene2", LoadSceneMode.Single);
 		}
 	}
