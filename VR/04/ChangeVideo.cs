a4231e405521a52385946a686777a879db4fb625
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2020-03-31 18:54:42 +0900

    Add files via upload

---
+++ VR/04/ChangeVideo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ChangeVideo : MonoBehaviour
+{
+    public VideoClip clip01;
+    public VideoClip clip02;
+    bool isClip02;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change02()
+    {
+        if (!isClip02)
+        {
+            gameObject.GetComponent<VideoPlayer>().clip = clip02;
+            isClip02 = true;
+        }
+    }
+}
---
+++ VR/04/RayClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1000;
+
+        if(Physics.Raycast(transform.position, forward, out hit))
+        {
+            Debug.Log("hit");
+            hit.transform.GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
