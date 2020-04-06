d82606645f457a8b5b128e1ad11da3c9ca81c16d
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2020-04-06 19:52:47 +0900

    Add files via upload

---
+++ VR/05/Asteroid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public GameObject explosion;
+    public AudioClip explosionSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Crash());
+    }
+
+    IEnumerator Crash()
+    {
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        this.gameObject.GetComponent<AudioSource>().PlayOneShot(explosionSound);
+        explosion.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        Destroy(this.gameObject);
+    }
+}
