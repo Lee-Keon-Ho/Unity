d50f2c7f0bfdaa6507ea23962197a07062a35622
Lee-Keon-Ho <55432783+Lee-Keon-Ho@users.noreply.github.com>
2020-04-10 14:59:11 +0900

    Add files via upload

---
+++ VR/assignment/04/ChangeVideo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ChangeVideo : MonoBehaviour
+{
+    public VideoClip clip01;
+    public VideoClip clip02;
+    bool isClip01;
+    bool isClip02;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Change01()
+    {
+        if (isClip01)
+        {
+            gameObject.GetComponent<VideoPlayer>().clip = clip01;
+            gameObject.GetComponent<VideoPlayer>().Play();
+            isClip01 = false;
+        }
+        else
+        {
+            gameObject.GetComponent<VideoPlayer>().Pause();
+            isClip01 = true;
+        }
+        isClip02 = true;
+    }
+    public void Change02()
+    {
+        if (isClip02)
+        {
+            gameObject.GetComponent<VideoPlayer>().clip = clip02;
+            gameObject.GetComponent<VideoPlayer>().Play();
+            isClip02 = false;
+        }
+        else
+        {
+            gameObject.GetComponent<VideoPlayer>().Pause();
+            isClip02 = true;
+        }
+        isClip01 = true;
+    }
+}
---
+++ VR/assignment/04/RayClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayClick : MonoBehaviour
+{
+    private float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 1000;
+
+        if(Physics.Raycast(transform.position,forward,out hit))
+        {
+            Timer += 1.0f / 2.0f * Time.deltaTime;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                 hit.transform.GetComponent<Button>().onClick.Invoke();
+            }
+
+            if (Timer >= 1.0f)
+            {
+                hit.transform.GetComponent<Button>().onClick.Invoke();
+                Timer = 0.0f;
+            }
+        }
+        else
+        {
+            Timer = 0.0f;
+        }
+    }
+}
